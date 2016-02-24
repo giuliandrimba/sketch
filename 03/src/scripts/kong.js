10cddc68991079ada3fec0271b821cccad97a875
Giulian Drimba <giuliandrimba@gmail.com>
2016-02-24 17:30:46 -0300

    hotfix

--- 03/src/scripts/kong.js
+++ 03/src/scripts/kong.js
@@ -138,8 +138,8 @@ function Kong(scene, camera, renderer) {
     self.mesh.geometry.verticesNeedUpdate = true;
 
     if(!animating) {
-      self.mesh.rotation.y += 0.1 * delta;
-      self.outerMesh.rotation.y += 0.1* delta;
+      self.mesh.rotation.y += 0.015 * delta;
+      self.outerMesh.rotation.y += 0.015* delta;
     }
 
   }
