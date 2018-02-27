c3893fa590e4fc1e0b4c44b4f753f8dcef3e2e68
MacInBox <macinbox@MacInBoxs-iMac.local>
2018-02-27 16:26:32 -0300

    Update proto 10 folder

--- proto-job-10/src/scripts/shader/old_velocity_fs.glsl
+++
@@ -1,7 +0,0 @@
-uniform vec2 positionsResolution;
-
-void main() {
-	vec2 uv = gl_FragCoord.xy / positionsResolution.xy;
-  vec3 newValue = texture2D(velocityTexture, uv).xyz;
-  gl_FragColor = vec4(vec3(newValue), 0.0);
-}
