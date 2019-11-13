2805300f2f14a308fa4726fd82488e2be2f5354f
Giulian Drimba <giulian.drimba@mediamonks.com>
2019-11-13 19:50:29 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const canvasSketch = require("canvas-sketch");
+
+// Sketch parameters
+const settings = {
+  dimensions: "a4",
+  pixelsPerInch: 300,
+  units: "in"
+};
+
+// Artwork function
+const sketch = () => {
+  return ({ context, width, height }) => {
+    // Margin in inches
+    const margin = 1 / 4;
+
+    // Off-white background
+    context.fillStyle = "hsl(0, 0%, 98%)";
+    context.fillRect(0, 0, width, height);
+
+    // Gradient foreground
+    const fill = context.createLinearGradient(0, 0, width, height);
+    fill.addColorStop(0, "cyan");
+    fill.addColorStop(1, "orange");
+
+    // Fill rectangle
+    context.fillStyle = fill;
+    context.fillRect(margin, margin, width - margin * 2, height - margin * 2);
+  };
+};
+
+// Start the sketch
+canvasSketch(sketch, settings);
